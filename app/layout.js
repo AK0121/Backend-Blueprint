4d31c7d4457a16fc37a1b51a6f92c9966a5772f3
AK0121 <9829859691abhishek@gmail.com>
2025-06-27 20:25:21 +0530

    Initial commit from Create Next App

---
+++ app/layout.js
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Backend Blueprint",
+  description: "Journey to the best",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
